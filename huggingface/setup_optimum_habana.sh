2526566c3ead7d9b57723b8e4617d8e598495608
Lau, Kiangpeng <kiangpeng.lau@intel.com>
2024-05-24 08:56:12 -0700

    Added optimum-habana setup scripts

---
+++ huggingface/setup_optimum_habana.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Clone optimum-habana
+# if [ ! -d "$(pwd)/optimum-habana" ]; then
+#     git clone https://github.com/huggingface/optimum-habana.git $(pwd)/optimum-habana
+# fi
+
+# Option 1: Use the latest stable release
+pip install --upgrade-strategy eager optimum[habana]
+# Option 2: Use the latest main branch under development
+pip install git+https://github.com/huggingface/optimum-habana.git
+# To use DeepSpeed on HPUs, you also need to run the following command:
+pip install git+https://github.com/HabanaAI/DeepSpeed.git@1.15.0
+
+# install requirements from each examples
